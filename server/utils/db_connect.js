128e9effdace90520c120b6f701259a99670853b
Diwansh Gupta <130465365+DiwanshGupta@users.noreply.github.com>
2024-09-13 01:06:32 +0530

    Update db_connect.js

--- server/utils/db_connect.js
+++ server/utils/db_connect.js
@@ -4,7 +4,7 @@ import {config}  from './config.js';
 const mongoUrl=config.mongoUrl;
 export const connectDB = async () => {
     try {
-        await mongoose.connect( mongoUrl);
+        await mongoose.connect('mongodb+srv://dewanshgupta120:eFADzJvPbLg741H6@echothreads.0bcwt.mongodb.net/');
         console.log( 'MongoDB connected' );
     } catch ( error ) {
         console.error( 'Error connecting to MongoDB:', error.message );
