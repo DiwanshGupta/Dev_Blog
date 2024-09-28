1981cf15d951b4b161200ac69344ef6e1750e968
Diwansh Gupta <130465365+DiwanshGupta@users.noreply.github.com>
2024-09-28 17:42:07 +0530

    Update store.js

--- client/redux/store.js
+++ client/redux/store.js
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import  userSlice  from './feature/user/slice';
 const store = configureStore({
   reducer: {
-    user: userSlice, // Replace this with your actual reducer
+    user: userSlice,  
   },
 });
 
